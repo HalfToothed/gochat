fa4ad0e394259fcea0b9d8a57b8795b2de4b112a
HalfToothed <abhishekandmails@gmail.com>
2024-11-22 02:18:35 +0530

    code cleanup

---
+++ client/main.js
@@ -0,0 +1,41 @@
+const Input = {};
+
+const messagesDiv = document.getElementById("messages");
+const messageInput = document.getElementById("message-input");
+const username = document.getElementById("username");
+const form = document.getElementById("input-form");
+
+// Connect to WebSocket server
+const ws = new WebSocket("ws://localhost:3000");
+
+// Handle form submission for text messages
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  if(!username.value || username.value.trim() === ""){
+    alert("Enter Username");
+    return;
+  }
+
+  if(!messageInput.value){
+    return;
+  }
+
+  Input.User = username.value;
+  Input.Text = messageInput.value;
+
+  let message = JSON.stringify(Input);
+
+  if (message) {
+    ws.send(message);
+    messageInput.value = "";
+  }
+});
+
+ws.onmessage = (event) => {
+  const newMessage = document.createElement("div");
+  newMessage.classList.add("message");
+  newMessage.textContent = event.data; // This should display the text
+  messagesDiv.appendChild(newMessage);
+  messagesDiv.scrollTop = messagesDiv.scrollHeight;
+};
